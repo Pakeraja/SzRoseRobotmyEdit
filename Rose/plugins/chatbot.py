5dafb0030e85720d0da3b12204c0db4b5d85a1fa
Iam Rocket <106468490+IamGroot321@users.noreply.github.com>
2022-07-02 00:15:58 +0530

    Update chatbot.py

--- Rose/plugins/chatbot.py
+++ Rose/plugins/chatbot.py
@@ -92,7 +92,7 @@ __MODULE__ = f"{Chat_Bot}"
 __HELP__ = """
 **Chatbot**
 
-AI based chatbot allows rose to talk and provides a more interactive group chat experience.
+AI based chatbot allows Bhanumathi to talk and provides a more interactive group chat experience.
 
 - /chatbot [ON/OFF]: Enables and disables Affiliate + AI Chat bot.
 
